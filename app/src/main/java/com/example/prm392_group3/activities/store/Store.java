65a6bb0cac4b0c672e922753c3632ed4706834a6
JC4F <lequan12122002@gmail.com>
2023-06-21 19:52:54 +0700

    update create bike

--- app/src/main/java/com/example/prm392_group3/activities/store/Store.java
+++ app/src/main/java/com/example/prm392_group3/activities/store/Store.java
@@ -3,14 +3,27 @@ package com.example.prm392_group3.activities.store;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.prm392_group3.R;
+import com.example.prm392_group3.adapters.BikeAdapter;
+import com.example.prm392_group3.models.Bike;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +31,11 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
  * create an instance of this fragment.
  */
 public class Store extends Fragment {
+
+    DatabaseReference myRef;
+    private List<Bike> bikeList;
+    private int commentCount;
+    private List<Integer> ratingList;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -62,14 +80,40 @@ public class Store extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        commentCount=0;
+        bikeList = new ArrayList<>();
+        ratingList = new ArrayList<>();
+
         View view = inflater.inflate(R.layout.fragment_store, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.store_recycle_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        BikeAdapter bikeAdapter = new BikeAdapter(getContext(), bikeList, commentCount, ratingList);
+        recyclerView.setAdapter(bikeAdapter);
+        myRef = FirebaseDatabase.getInstance().getReference("Bike");
+
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                bikeList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Bike bike = snapshot.getValue(Bike.class);
+                    bikeList.add(bike);
+                }
+                bikeAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle error
+            }
+        });
 
         FloatingActionButton addStoreFab = view.findViewById(R.id.add_store_fab);
         addStoreFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // getActivity(): truy cập đến Activity chứa Fragment đó
                 Intent intent = new Intent(getActivity(), AddBike.class);
                 startActivity(intent);
             }
---
+++ app/src/main/java/com/example/prm392_group3/adapters/BikeAdapter.java
@@ -0,0 +1,89 @@
+package com.example.prm392_group3.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.prm392_group3.R;
+import com.example.prm392_group3.models.Bike;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+import java.util.Locale;
+
+public class BikeAdapter extends RecyclerView.Adapter<BikeAdapter.ViewHolder> {
+
+    private List<Bike> bikeList;
+    private int commentCount;
+    private List<Integer> ratingList;
+    private Context context;
+
+    public BikeAdapter(Context context, List<Bike> bikeList, int commentCount, List<Integer> ratingList) {
+        this.context = context;
+        this.bikeList = bikeList;
+        this.commentCount = commentCount;
+        this.ratingList = ratingList;
+    }
+
+    @NonNull
+    @Override
+    public BikeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate layout cho mỗi item trong danh sách
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.store_bike_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BikeAdapter.ViewHolder holder, int position) {
+        // Lấy dữ liệu từ danh sách dựa trên vị trí position
+        Bike bike = bikeList.get(position);
+
+        // Load hình ảnh từ URL và hiển thị nó trong ImageView
+        Picasso.get().load(bike.getImageUrl()).into(holder.bikeImage);
+        holder.bikeName.setText(bike.getName());
+        holder.remainQuantity.setText("Remain quantity: " + bike.getQuantity());
+        holder.ratingsCount.setText(String.format("%.1f (%d ratings)", calculateAverageRating(), ratingList.size()));
+        holder.commentsCount.setText(String.valueOf(commentCount));
+        holder.bikeStatus.setText(bike.isAvailable() ? "Available" : "Unavailable");
+    }
+
+    private float calculateAverageRating() {
+        int sum = 0;
+        if(ratingList.size()==0) return 0;
+        for (int rating : ratingList) {
+            sum += rating;
+        }
+        return (float) sum / ratingList.size();
+    }
+
+    @Override
+    public int getItemCount() {
+        return bikeList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView bikeImage;
+        TextView bikeName;
+        TextView remainQuantity;
+        TextView ratingsCount;
+        TextView commentsCount;
+        TextView bikeStatus;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            bikeImage = itemView.findViewById(R.id.store_item_image);
+            bikeName = itemView.findViewById(R.id.store_bike_name);
+            remainQuantity = itemView.findViewById(R.id.store_bike_remain_quantity);
+            ratingsCount = itemView.findViewById(R.id.store_ratings_count);
+            commentsCount = itemView.findViewById(R.id.store_comments_count);
+            bikeStatus = itemView.findViewById(R.id.store_bike_status);
+        }
+    }
+}
+
