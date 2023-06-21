306b6ed822b8b072a56aeb9c0024e8145aa96d14
Truong Pham <quansuhongtruong@gmail.com>
2023-06-21 10:33:54 +0700

    NewsPage

--- app/src/main/java/com/example/prm392_group3/activities/blog/AllBlog.java
+++ app/src/main/java/com/example/prm392_group3/activities/blog/AllBlog.java
@@ -4,11 +4,29 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.prm392_group3.R;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.prm392_group3.activities.MainActivity;
+import com.example.prm392_group3.models.News;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.example.prm392_group3.adapters.NewsAdapter;
+
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +34,9 @@ import com.example.prm392_group3.R;
  * create an instance of this fragment.
  */
 public class AllBlog extends Fragment {
+    private RecyclerView recyclerView;
+    private NewsAdapter adapter;
+    private List<News> newsList;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +82,24 @@ public class AllBlog extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_blog, container, false);
+        View view = inflater.inflate(R.layout.fragment_all_blog, container, false);
+
+        recyclerView = view.findViewById(R.id.recy_news);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // Create a list of news items
+        newsList = new ArrayList<>();
+        // Add your news items to the list
+        // Example:
+        newsList.add(new News(1, "2023-06-20", "Exciting News 1", "https://example.com/news1", "This is the content of News 1", "News Source 1", "https://example.com/source1", "news1_image.jpg"));
+        newsList.add(new News(2, "2023-06-19", "Breaking News 2", "https://example.com/news2", "This is the content of News 2", "News Source 2", "https://example.com/source2", "news2_image.jpg"));
+        newsList.add(new News(3, "2023-06-18", "Latest News 3", "https://example.com/news3", "This is the content of News 3", "News Source 3", "https://example.com/source3", "news3_image.jpg"));
+        // ...
+
+        adapter = new NewsAdapter(newsList);
+        Log.i("info",""+recyclerView);
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 }
