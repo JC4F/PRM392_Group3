93362d89e70cd579818f7c1abb77d2913cbddcfa
phtphtxxzz <quansuhongtruong@gmail.com>
2023-07-17 02:32:20 +0700

    last

--- app/src/main/java/com/example/prm392_group3/adapters/CommentAdapter.java
+++ app/src/main/java/com/example/prm392_group3/adapters/CommentAdapter.java
@@ -16,6 +16,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.example.prm392_group3.models.Comment;
+import com.example.prm392_group3.models.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.text.ParseException;
@@ -27,6 +34,8 @@ import java.util.Locale;
 public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentViewHolder> {
     private List<Comment> commentList;  // Assuming you have a list of CommentItem objects
     private  Context context;
+    DatabaseReference userRef;
+    FirebaseDatabase database;
 
     public CommentAdapter(List<Comment> commentList, Context context) {
         this.commentList = commentList;
@@ -43,10 +52,32 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.CommentV
     @Override
     public void onBindViewHolder(@NonNull CommentViewHolder holder, int position) {
         Comment commentItem = commentList.get(position);
+        database = FirebaseDatabase.getInstance();
+        userRef = database.getReference("User");
+        Query userQuery = userRef.orderByChild("id").equalTo(commentItem.getUserId());
+        userQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    User user = userSnapshot.getValue(User.class);
+                    if (user != null) {
+                        // Here, you have access to the user with the ID news.getCreatedBy()
+                        // You can do something with the user object
+                        // For example, set the source TextView to the user's name
+                        holder.commentUserName.setText(user.getUsername().length()<=30?"By: "+user.getUsername():"By: "+user.getUsername().substring(0, 30)+"...");
+                    }
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(context, "Failed to load user details", Toast.LENGTH_SHORT).show();
+                holder.commentUserName.setText("By "+"?");
+
+            }
+        });
         // Bind the data to the views in the CommentViewHolder
         holder.commentDes.setText(commentItem.getCommentContent().length()<=30?commentItem.getCommentContent():commentItem.getCommentContent().substring(0, 30)+"...");
-        holder.commentUserName.setText(commentItem.getUserName().length()<=30?"User: "+commentItem.getUserName():"User: "+commentItem.getUserName().substring(0, 30)+"...");
         SimpleDateFormat inputDateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.ENGLISH);
         SimpleDateFormat outputDateFormat = new SimpleDateFormat("MM-dd-yyyy", Locale.ENGLISH);
 
